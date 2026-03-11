6bd53f19182a4d151e4ff822ccb81079f211a149
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2026-03-11 09:13:20 -0500

    Update app.h

--- code/logic/fossil/code/app.h
+++ code/logic/fossil/code/app.h
@@ -1,23 +1,40 @@
-/*
+/**
  * -----------------------------------------------------------------------------
  * Project: Fossil Logic
  *
- * This file is part of the Fossil Logic project, which aims to develop high-
- * performance, cross-platform applications and libraries. The code contained
- * herein is subject to the terms and conditions defined in the project license.
+ * This file is part of the Fossil Logic project, which aims to develop
+ * high-performance, cross-platform applications and libraries. The code
+ * contained herein is licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain
+ * a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  *
  * Author: Michael Gene Brockus (Dreamer)
+ * Date: 04/05/2014
  *
- * Copyright (C) 2024 Fossil Logic. All rights reserved.
+ * Copyright (C) 2014-2025 Fossil Logic. All rights reserved.
  * -----------------------------------------------------------------------------
  */
 #ifndef FOSSIL_APP_CODE_H
 #define FOSSIL_APP_CODE_H
 
 #include "common.h"
+#include "commands.h"
+#include "magic.h"
+
+#define FOSSIL_APP_NAME "Squid Tool"
+#define FOSSIL_APP_VERSION "0.1.0"
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 extern int FOSSIL_IO_VERBOSE; // Verbose output flag
@@ -25,12 +42,12 @@ extern int FOSSIL_IO_VERBOSE; // Verbose output flag
 /**
  * @brief Custom application entry point.
  * This function is the main entry point for the custom application.
- * 
+ *
  * @param argc The number of command-line arguments.
  * @param argv The array of command-line arguments.
  * @return 0 if the application runs successfully, non-zero otherwise.
  */
-bool app_entry(int argc, char** argv);
+bool app_entry(int argc, char **argv);
 
 #ifdef __cplusplus
 }
